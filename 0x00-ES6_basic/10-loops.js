46109592a43e1f1fb64c02543a31d9e6d008aff2
yeshan111 <yeshan111@gmail.com>
2023-03-20 05:06:10 -0700

    whatis even

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+
+export default function appendToEachArrayValue(array, appendString) {
+  const returnArray = [];
+  for (const element of array) {
+    returnArray.push(appendString + element);
+  }
+
+  return returnArray;
+}
